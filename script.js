6ce54a053af401ee0cc9473e7cf1c9bbf859a690
Hadas89 <Hadas89@gmail.com>
2019-06-24 22:41:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+window.onload = function() {
+    console.log('hello');
+  };
+
+  var todo = {
+    addToList: function() {
+      var listitemvalue = document.querySelector('#list-item');
+      var btn = document.querySelector('.add-item');
+      btn.addEventListener('click', function() {
+        var listitem = document.createElement('li');
+        listitem.className = 'item';
+
+        document.getElementsByClassName('list')[0].appendChild(listitem);
+        listitem.innerHTML = listitemvalue.value;
+
+        var listitems = document.querySelectorAll('.item');
+      //convert nodeList to array
+      var listitemsarr = Array.from(listitems);
+      listitemsarr.push(listitem);
+     console.log(listitemsarr);
+
+     listitemsarr.forEach(function(element) {
+        element.addEventListener('click', function() {
+          this.style.textDecorationLine = 'line-through';
+          this.style.color = 'grey';
+          console.log(listitemsarr);
+        });
+     
+    })
+   
+      });
+     
+},
+reset:function () {
+    var resetbtn = document.getElementsByClassName('reset-button')[0];
+    resetbtn.addEventListener('click', function () {
+        var listitems = document.querySelectorAll('.list li');
+        for (let i = 0; i <listitems.length; i++) {
+          listitems[i].innerHTML = '';
+            
+        }
+    })
+}
+
+
+}
+
+
+
+
+  todo.addToList();
+  todo.reset()
+ 
+
+
+  
+
