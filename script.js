6f7706ed6aceca891eb25c146867e459a8e6f567
Hadas89 <Hadas89@gmail.com>
2019-06-25 20:48:05 +0300

    adding responsiveness

--- script.js
+++ script.js
@@ -1,57 +1,42 @@
 window.onload = function() {
-    console.log('hello');
-  };
+  console.log('hello');
+};
 
-  var todo = {
-    addToList: function() {
-      var listitemvalue = document.querySelector('#list-item');
-      var btn = document.querySelector('.add-item');
-      btn.addEventListener('click', function() {
-        var listitem = document.createElement('li');
-        listitem.className = 'item';
+var todo = {
+  addToList: function() {
+    var listitemvalue = document.querySelector('#list-item');
+    var btn = document.querySelector('.add-item');
+    btn.addEventListener('click', function() {
+      var listitem = document.createElement('li');
+      listitem.className = 'item';
 
-        document.getElementsByClassName('list')[0].appendChild(listitem);
-        listitem.innerHTML = listitemvalue.value;
+      document.getElementsByClassName('list')[0].appendChild(listitem);
+      listitem.innerHTML = listitemvalue.value;
 
-        var listitems = document.querySelectorAll('.item');
+      var listitems = document.querySelectorAll('.item');
       //convert nodeList to array
       var listitemsarr = Array.from(listitems);
       listitemsarr.push(listitem);
-     console.log(listitemsarr);
+      console.log(listitemsarr);
 
-     listitemsarr.forEach(function(element) {
+      listitemsarr.forEach(function(element) {
         element.addEventListener('click', function() {
-          this.style.textDecorationLine = 'line-through';
-          this.style.color = 'grey';
-          console.log(listitemsarr);
+          console.log(element);
+          this.classList.add('complete');
         });
-     
-    })
-   
       });
-     
-},
-reset:function () {
+    });
+  },
+  reset: function() {
     var resetbtn = document.getElementsByClassName('reset-button')[0];
-    resetbtn.addEventListener('click', function () {
-        var listitems = document.querySelectorAll('.list li');
-        for (let i = 0; i <listitems.length; i++) {
-          listitems[i].innerHTML = '';
-            
-        }
-    })
-}
+    resetbtn.addEventListener('click', function() {
+      var listitems = document.querySelectorAll('.list li');
+      for (let i = 0; i < listitems.length; i++) {
+        listitems[i].innerHTML = '';
+      }
+    });
+  }
+};
 
-
-}
-
-
-
-
-  todo.addToList();
-  todo.reset()
- 
-
-
-  
-
+todo.addToList();
+todo.reset();
